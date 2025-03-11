ba9f3e53674dd7a12425bc1de925b572fdb4640d
giwon1130 <gwim1130@si-analytics.ai>
2025-03-11 10:45:57 +0900

    fix: JitPack에서 Spring Boot Plugin 빌드 문제 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     application
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.3.6"
+    id("org.springframework.boot") version "3.3.6" apply false
     id("io.spring.dependency-management") version "1.1.7"
 }
 
